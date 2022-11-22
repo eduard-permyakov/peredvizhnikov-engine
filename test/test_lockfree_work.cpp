238ac28875b0d6576bf2da777d7987191b38c9e7
Eduard Permyakov <edward.permyakov@gmail.com>
2022-11-22 20:51:21 +0700

    Add tests for lockfree parallel work

--- test/test_lockfree_work.cpp
+++ test/test_lockfree_work.cpp
@@ -1,4 +1,5 @@
 import lockfree_work;
+import iterable_lockfree_list;
 import logger;
 import assert;
 
@@ -8,6 +9,8 @@ import <future>;
 
 
 constexpr int kNumSteps = 100;
+constexpr int kNumParallelWorkItems = 100;
+constexpr int kNumParallelWorkers = 10;
 
 struct Object
 {
@@ -36,6 +39,11 @@ struct Object
             && (d == rhs.d)
             && (e == rhs.e)
             && (f == rhs.f);
+    }
+
+    std::strong_ordering operator<=>(const Object& rhs) const
+    {
+        return (a <=> rhs.a);
     }
 };
 
@@ -69,6 +77,96 @@ void test_lfsw()
     pe::assert(result == expected, "", __FILE__, __LINE__);
 }
 
+template <typename Work>
+void lfpw_worker(Work& work)
+{
+    work.Complete();
+}
+
+void test_lfpw()
+{
+    std::array<Object, kNumParallelWorkItems> objects{};
+    for(int i = 0; i < std::size(objects); i++) {
+        objects[i] = {i, i + 1, i + 2, i + 3, i + 4, i + 5};
+    }
+
+    struct TaggedObject
+    {
+        int    id;
+        Object object;
+
+        bool operator==(const TaggedObject& rhs) const
+        {
+            return (id == rhs.id);
+        }
+
+        std::strong_ordering operator<=>(const TaggedObject& rhs) const
+        {
+            return (id <=> rhs.id);
+        }
+    };
+
+    struct ObjectStepWorkItem
+    {
+        TaggedObject tagged;
+        int          i;
+    };
+
+    std::vector<std::future<void>> tasks{};
+    std::array<ObjectStepWorkItem, std::size(objects)> input{};
+    for(int i = 0; i < std::size(input); i++) {
+        input[i] = {{i, objects[i]}, i};
+    }
+
+    /* The list behaves as a set, thereby discarding 
+     * duplicate insertions for a work item.
+     */
+    pe::IterableLockfreeList<TaggedObject> results{};
+
+    pe::LockfreeParallelWork work{
+        input, results,
+        +[](const ObjectStepWorkItem& work, pe::IterableLockfreeList<TaggedObject>& out) {
+            Object result = work.tagged.object;
+            result.step(work.i);
+            out.Insert({work.tagged.id, result});
+        }
+    };
+
+    for(int i = 0; i < kNumParallelWorkers; i++) {
+        tasks.push_back(
+            std::async(std::launch::async, lfpw_worker<decltype(work)>, 
+            std::ref(work)));
+    }
+
+    /* We don't have to wait for the threads to complete.
+     * The 'awaiting' thread will help out in the work
+     * and is able to 'steal' work from any preempted
+     * thread.
+     */
+    work.Complete();
+
+    for(int i = 0; i < std::size(objects); i++) {
+        Object result = input[i].tagged.object;
+        result.step(input[i].i);
+        objects[i] = result;
+    }
+    std::sort(objects.begin(), objects.end());
+
+    auto results_snapshot = results.TakeSnapshot();
+    std::sort(results_snapshot.begin(), results_snapshot.end(),
+        [](TaggedObject a, TaggedObject b){ return a.object < b.object; });
+
+    pe::assert(results_snapshot.size() == objects.size());
+
+    for(int i = 0; i < kNumParallelWorkItems; i++) {
+        pe::assert(objects[i] == results_snapshot[i].object);
+    }
+
+    for(const auto& task : tasks) {
+        task.wait();
+    }
+}
+
 int main()
 {
     int ret = EXIT_SUCCESS;
@@ -78,6 +176,10 @@ int main()
         pe::ioprint(pe::TextColor::eGreen, "Testing Lock-Free Serial Work.");
         test_lfsw();
         pe::ioprint(pe::TextColor::eGreen, "Finished Lock-Free Serial Work test.");
+
+        pe::ioprint(pe::TextColor::eGreen, "Testing Lock-Free Parallel Work.");
+        test_lfpw();
+        pe::ioprint(pe::TextColor::eGreen, "Finished Lock-Free Parallel Work test.");
 
     }catch(std::exception &e){
 
