d62b503e7c76ea71419051f98249171993c11656
Eduard Permyakov <edward.permyakov@gmail.com>
2022-12-02 20:30:22 +0700

    whitespace fix

--- src/lockfree_queue.cpp
+++ src/lockfree_queue.cpp
@@ -16,7 +16,7 @@ namespace pe{
 export
 template <typename T>
 requires (std::is_default_constructible_v<T> 
-	  && (std::is_copy_constructible_v<T> || std::is_move_constructible_v<T>))
+      && (std::is_copy_constructible_v<T> || std::is_move_constructible_v<T>))
 class LockfreeQueue
 {
 private:
@@ -68,8 +68,8 @@ public:
             value = Dequeue();
         }while(value.has_value());
 
-		Node *sentinel = m_head.Load(std::memory_order_relaxed).m_ptr;
-		delete sentinel;
+        Node *sentinel = m_head.Load(std::memory_order_relaxed).m_ptr;
+        delete sentinel;
     }
 
     template <typename U = T>
@@ -88,18 +88,18 @@ public:
             next = tail.m_ptr->m_next.Load(std::memory_order_acquire);
             if(next.m_ptr == nullptr) {
 
-				AnnotateHappensBefore(__FILE__, __LINE__, &tail.m_ptr->m_next);
+                AnnotateHappensBefore(__FILE__, __LINE__, &tail.m_ptr->m_next);
                 if(tail.m_ptr->m_next.CompareExchange(next, {node, next.m_count + 1},
                     std::memory_order_release, std::memory_order_relaxed))
                     break;
             }else{
 
-				AnnotateHappensBefore(__FILE__, __LINE__, &m_tail);
+                AnnotateHappensBefore(__FILE__, __LINE__, &m_tail);
                 m_tail.CompareExchange(tail, {next.m_ptr, tail.m_count + 1},
                     std::memory_order_release, std::memory_order_relaxed);
             }
         }
-		AnnotateHappensBefore(__FILE__, __LINE__, &m_tail);
+        AnnotateHappensBefore(__FILE__, __LINE__, &m_tail);
         m_tail.CompareExchange(tail, {node, tail.m_count + 1},
             std::memory_order_release, std::memory_order_relaxed);
     }
@@ -118,7 +118,7 @@ public:
 
             tail = m_tail.Load(std::memory_order_acquire);
             next = head.m_ptr->m_next.Load(std::memory_order_acquire);
-			AnnotateHappensAfter(__FILE__, __LINE__, &head.m_ptr->m_next);
+            AnnotateHappensAfter(__FILE__, __LINE__, &head.m_ptr->m_next);
 
             auto next_hazard = m_hp.AddHazard(1, next.m_ptr);
             if(head != m_head.Load(std::memory_order_relaxed))
@@ -139,7 +139,7 @@ public:
              * CAS succeeds.
              */
             ret = next.m_ptr->m_value;
-			AnnotateHappensBefore(__FILE__, __LINE__, &m_head);
+            AnnotateHappensBefore(__FILE__, __LINE__, &m_head);
 
             if(m_head.CompareExchange(head, {next.m_ptr, head.m_count + 1},
                 std::memory_order_release, std::memory_order_relaxed))
@@ -152,7 +152,7 @@ public:
 
 template <typename T>
 requires (std::is_default_constructible_v<T> 
-	  && (std::is_copy_constructible_v<T> || std::is_move_constructible_v<T>))
+      && (std::is_copy_constructible_v<T> || std::is_move_constructible_v<T>))
 bool LockfreeQueue<T>::Pointer::operator==(const Pointer& rhs) const
 {
    return (m_ptr == rhs.m_ptr) && (m_count == rhs.m_count);
@@ -160,7 +160,7 @@ bool LockfreeQueue<T>::Pointer::operator==(const Pointer& rhs) const
 
 template <typename T>
 requires (std::is_default_constructible_v<T> 
-	  && (std::is_copy_constructible_v<T> || std::is_move_constructible_v<T>))
+      && (std::is_copy_constructible_v<T> || std::is_move_constructible_v<T>))
 bool LockfreeQueue<T>::Pointer::operator!=(const Pointer& rhs) const
 {
   return !operator==(rhs);
