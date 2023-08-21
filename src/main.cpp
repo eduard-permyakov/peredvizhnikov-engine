93a549000cf84af7012809cfab4d11eb9b590869
Eduard Permyakov <edward.permyakov@gmail.com>
2023-08-21 17:49:14 +0300

    main.cpp: Override global new and delete operators

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@ import engine;
 import logger;
 import event;
 import SDL2;
+import alloc;
 
+import <new>;
 import <cstdlib>;
 import <exception>;
 import <stdexcept>;
+import <algorithm>;
 
 struct SDL2Context
 {
@@ -21,6 +24,58 @@ struct SDL2Context
         SDL_Quit();
     }
 };
+
+void *operator new(std::size_t size) noexcept(false)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = alloc.NextAlignedBlockSize(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+    return alloc.Allocate(size);
+}
+
+void *operator new[](std::size_t size) noexcept(false)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = alloc.NextAlignedBlockSize(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+    return alloc.Allocate(size);
+}
+
+void *operator new(std::size_t size, std::align_val_t align) noexcept(false)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = std::max(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+    return alloc.AllocateAligned(size, align);
+}
+
+void *operator new[](std::size_t size, std::align_val_t align) noexcept(false)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = std::max(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+    return alloc.AllocateAligned(size, align);
+}
+
+void operator delete(void *ptr) noexcept
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    alloc.Free(ptr);
+}
+
+void operator delete[](void *ptr) noexcept
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    alloc.Free(ptr);
+}
+
+void operator delete(void* ptr, std::align_val_t align) noexcept
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    alloc.FreeAligned(ptr, align);
+}
+
+void operator delete[](void* ptr, std::align_val_t align) noexcept
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    alloc.FreeAligned(ptr, align);
+}
 
 int main()
 {
