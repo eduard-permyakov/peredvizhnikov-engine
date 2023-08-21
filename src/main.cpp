69178fa3b8889e59f29fd4b5a8fe18de693019f3
Eduard Permyakov <edward.permyakov@gmail.com>
2023-08-21 23:45:24 +0300

    Override libc allocation functions with custom allocator

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ import logger;
 import event;
 import SDL2;
 import alloc;
+import unistd;
 
 import <new>;
 import <cstdlib>;
@@ -11,15 +12,15 @@ import <exception>;
 import <stdexcept>;
 import <algorithm>;
 
-struct SDL2Context
+struct SDLContext
 {
-    SDL2Context()
+    SDLContext()
     {
         if(SDL_Init(SDL_INIT_VIDEO) < 0)
-            throw std::runtime_error{"Failed to initialize SDL2."};
+            throw std::runtime_error{"Failed to initialize SDL."};
     }
 
-    ~SDL2Context()
+    ~SDLContext()
     {
         SDL_Quit();
     }
@@ -42,14 +43,12 @@ void *operator new[](std::size_t size) noexcept(false)
 void *operator new(std::size_t size, std::align_val_t align) noexcept(false)
 {
     pe::Allocator& alloc = pe::Allocator::Instance();
-    size = std::max(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
     return alloc.AllocateAligned(size, align);
 }
 
 void *operator new[](std::size_t size, std::align_val_t align) noexcept(false)
 {
     pe::Allocator& alloc = pe::Allocator::Instance();
-    size = std::max(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
     return alloc.AllocateAligned(size, align);
 }
 
@@ -77,12 +76,94 @@ void operator delete[](void* ptr, std::align_val_t align) noexcept
     alloc.FreeAligned(ptr, align);
 }
 
+void *malloc(size_t size)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = alloc.NextAlignedBlockSize(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+    return alloc.Allocate(size);
+}
+
+void *calloc(size_t num, size_t size)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = alloc.NextAlignedBlockSize(num * size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+    auto ret = alloc.Allocate(size);
+    std::memset(ret, 0, size);
+    return ret;
+}
+
+void *realloc(void *ptr, size_t size)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = alloc.NextAlignedBlockSize(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+    auto ret = alloc.Allocate(size);
+    if(ptr) {
+        std::size_t copy_size = std::min(size, alloc.AllocationSize(ptr));
+        std::memcpy(ret, ptr, copy_size);
+        alloc.Free(ptr);
+    }
+    return ret;
+}
+
+void *memalign(size_t alignment, size_t size)
+{
+    if(alignment > pe::kMaxBlockSize)
+        return nullptr;
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    size = alloc.NextAlignedBlockSize(size, alignment);
+    return alloc.Allocate(size);
+}
+
+int posix_memalign(void **memptr, size_t alignment, size_t size)
+{
+    if(size == 0) {
+        *memptr = nullptr;
+        return 0;
+    }
+
+    void *ret = memalign(alignment, size);
+    if(!ret)
+        return -ENOMEM;
+
+    *memptr = ret;
+    return 0;
+}
+
+void *aligned_alloc(size_t alignment, size_t size)
+{
+    return memalign(alignment, size);
+}
+
+void *valloc(size_t size)
+{
+    return memalign(getpagesize(), size);
+}
+
+void *pvalloc(size_t size)
+{
+    size_t page_size = getpagesize();
+    size = std::ceil(size / ((float)page_size)) * page_size;
+    return memalign(page_size, size);
+}
+
+size_t malloc_usable_size(void *ptr)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    return alloc.AllocationSize(ptr);
+}
+
+void free(void *ptr)
+{
+    pe::Allocator& alloc = pe::Allocator::Instance();
+    alloc.Free(ptr);
+}
+
 int main()
 {
     int ret = EXIT_SUCCESS;
     try{
 
-        SDL2Context sdl;
+        SDLContext sdl;
         pe::Scheduler scheduler{};
         auto engine = pe::Engine::Create(scheduler, pe::Priority::eCritical,
             pe::CreateMode::eLaunchAsync, pe::Affinity::eAny);
